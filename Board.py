88544e88cd186cc01e73bc9c8c0d07fc01c660f0
P0NYB0Y <will.a.schreiner@gmail.com>
2021-07-15 00:05:46 -0400

    Separated classes into modules

---
+++ Board.py
@@ -0,0 +1,117 @@
+from Square import *
+
+class board:
+    def __init__(self, table_in = None):
+        if (table_in == None):
+            self.table = [[],[],[],[],[],[],[],[],[]]
+        else:
+            self.table = table_in
+
+    # User input/output functions
+    def manual_fill_table(self):    # Allows user to fill out table manually
+        print ("Fill the table by row moving from left to right; use zero for squares which are not filled in.")
+        for x in range(9):
+            correct, input_valid = False, False
+            while correct == False:
+                print ("Filling row", x + 1)
+                for y in range(9):  # TODO: Add input verification and maybe have user input "?" instead of zero
+                    usr_val_in = input("Input number: ")
+                    self.table[x].append(square(usr_val_in))
+                while input_valid == False:
+                    print ("Is", self.table[x], "correct? (y/n)")
+                    usr_in = input()
+                    if usr_in.upper() == "Y" or usr_in.upper() == "N":
+                        input_valid = True
+                    else:
+                        print ("Invalid Input, Try Again.")
+                if usr_in.upper() == "N":
+                    self.table[x].clear()
+                    print ("Fill Row Again.")
+                    input_valid = False
+                else:
+                    correct = True
+        return
+
+    def print_table(self):  # Could remove ? cases with use of get value function in square class
+        horiz = 0
+        for i in self.table:
+            vert = 0
+            for j in i:
+                vert += 1
+                if (vert % 3 == 0 and vert < 9):
+                    if j.value == False:
+                        print ("?","", end="")
+                    else:
+                        print (j.value,"", end="")
+                    print ("|",end=" ")
+                else:
+                    if j.value == False:    # If value is 0 (unknown) then display as "?"
+                        print ("?","  ", end="")
+                    else:
+                        print (j.value,"  ", end="")
+            horiz += 1
+            if (horiz % 3 == 0 and horiz < 9):
+                print ("\n")
+                for k in range(33):
+                    print ("-",end="")
+            print("\n")
+        print ("\n")
+        return
+    
+    # Sudoku solving functions (return true/false to make sure that)
+    def fill_squares(self):
+        change = False
+        for i in self.table:
+            for j in i:
+                if len(j.pos) == 1: #if there is only one item in list
+                    j.set_val(j.pos[0])
+                    change = True
+        return change
+
+    def hor_comp(self):
+        for i in self.table:
+            temp_table = []
+            for j in i:
+                if not len(j.pos):
+                    temp_table.append(j.value)
+            for k in i:
+                k.pos = [x for x in k.pos if x not in temp_table]   # TODO: test check else if continue makes faster
+        return self.fill_squares()
+    
+    def vert_comp(self):    # Is there a cleaner way?
+        for x in range(9):
+            temp_table = []
+            for i in self.table:
+                if not len(i[x].pos):
+                    temp_table.append(i[x].value)
+            for i in self.table:
+                if len(i[x].pos) > 1:
+                    i[x].pos = [x for x in i[x].pos if x not in temp_table]
+        return self.fill_squares()
+
+    def square_check(self): #Issue here
+        hor_index, vert_index = [0,3,6], [0,3,6] #TODO: find better syntax
+        for h_start in hor_index:
+            square_lst = []
+            for v_start in vert_index:
+                for x in range(3):
+                    for y in range (3):
+                        if (self.table[h_start + x][v_start + y].value != 0):
+                            square_lst.append(self.table[h_start + x][v_start + y].value)
+            for v_start in vert_index:
+                for x in range(3):
+                    for y in range (3):
+                        self.table[h_start + x][v_start + y].pos = [x for x in self.table[h_start + x][v_start + y].pos if x not in square_lst]
+        return  self.fill_squares()
+
+    # def num_inst_chk(self): # Checks by number
+    #     for x in range(1,10):
+    #         for i in self.table: # Horizontal check
+                
+    #             # num_of_instance = 0
+    #             # for j in i:
+    #             #     if j.value == x:
+    #             #         return #Add break or something here
+    #             #     if x in j.pos:
+    #             #         num_of_instance += 1
+    #             return self.fill_squares()
---
+++ Square.py
@@ -0,0 +1,17 @@
+class square:
+    def __init__(self, val_in = False):
+        if val_in == False:
+            self.pos = list(range(1,10))
+        else:    
+            self.pos = []   # Using empty adds another easy way to detect finished squares
+        self.value = val_in
+
+    def __repr__(self): #Used so manual fill can easily display rows
+        if self.value == 0:
+            return "?"
+        else:
+            return str(self.value)
+
+    def set_val (self, val_in): #Used to set value while solving
+        self.pos = []
+        self.value = val_in
