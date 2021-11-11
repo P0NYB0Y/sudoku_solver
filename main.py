0cd1e0b7eec8d3eb378f93ac314b1020d51ee5db
P0NYB0Y <will.a.schreiner@gmail.com>
2021-11-11 12:48:05 -0500

    Cleaned up for demo purposes

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from Square import square
 from Display import board_UI
 from tkinter import Tk, Canvas, Frame, Button, BOTH, TOP, BOTTOM
 
-# full_test_table = ([square(),square(),square(),square(),square(),square(),square(),square(),square()],
+# full_test_board = ([square(),square(),square(),square(),square(),square(),square(),square(),square()],
 # [square(),square(),square(),square(),square(),square(),square(),square(),square()],
 # [square(),square(),square(),square(),square(),square(),square(),square(),square()],
 # [square(),square(),square(),square(),square(),square(),square(),square(),square()],
@@ -13,30 +13,21 @@ from tkinter import Tk, Canvas, Frame, Button, BOTH, TOP, BOTTOM
 # [square(),square(),square(),square(),square(),square(),square(),square(),square()],
 # [square(),square(),square(),square(),square(),square(),square(),square(),square()])
 
-full_test_table = (
-[square(),square(),square(),square(),square(9),square(),square(),square(),square(8)],
-[square(),square(),square(),square(),square(3),square(),square(1),square(7),square()],
-[square(),square(),square(),square(7),square(),square(),square(3),square(6),square()],
-[square(),square(6),square(),square(),square(),square(8),square(),square(),square()],
-[square(1),square(8),square(),square(4),square(),square(),square(),square(),square()],
-[square(),square(),square(),square(5),square(),square(),square(9),square(),square(2)],
-[square(8),square(),square(),square(),square(1),square(3),square(),square(),square()],
-[square(),square(4),square(),square(),square(),square(),square(),square(),square()],
-[square(5),square(),square(),square(),square(),square(),square(),square(2),square(7)])
+test_board = ([square(),square(),square(5),square(),square(7),square(8),square(),square(),square()],
+                   [square(),square(7),square(),square(4),square(),square(),square(),square(),square()],
+                   [square(2),square(),square(),square(5),square(6),square(1),square(3),square(7),square(4)],
+                   [square(),square(),square(),square(),square(),square(),square(),square(),square()],
+                   [square(),square(),square(),square(),square(4),square(5),square(2),square(1),square(8)],
+                   [square(),square(),square(),square(),square(3),square(2),square(4),square(9),square()],
+                   [square(),square(),square(),square(9),square(),square(),square(),square(),square(1)],
+                   [square(3),square(),square(1),square(),square(),square(7),square(6),square(),square()],
+                   [square(4),square(),square(9),square(),square(),square(),square(8),square(),square()])
+full_test_board = board(test_board)
 
-full_test_board = board(full_test_table)
-root = Tk()
-board_UI(root,full_test_board)
+for z in range (100):
+    full_test_board.hor_comp()
+    full_test_board.vert_comp()
+    full_test_board.square_check()
+    full_test_board.num_inst_chk()
 
-
-# for z in range (5):
-#     full_test_board.hor_comp()
-#     full_test_board.vert_comp()
-#     full_test_board.square_check()
-#     full_test_board.num_inst_chk()
-#     print("Num_Instance")
-#     full_test_board.print_table()
-#     full_test_board.naked_matches()
-#     print()
-#     print("Obv Pairs")
-#     full_test_board.print_table()
+full_test_board.print_table()
